cce7040e48a9eca669bdcbe14bfcc7c53184f026
Thomas Wendt <thoemy@gmx.net>
2012-04-05 19:25:19 +0200

    msm: bravo: add microp and mcc files

--- include/linux/bma150.h
+++ include/linux/bma150.h
@@ -13,6 +13,34 @@
 #ifndef LINUX_BMA150_MODULE_H
 #define LINUX_BMA150_MODULE_H
 
+#define BMA150_I2C_NAME "bma150"
+#ifdef CONFIG_SPI_QSD
+#define BMA150_G_SENSOR_NAME "bma150_uP_spi"
+#else
+#define BMA150_G_SENSOR_NAME "bma150"
+#endif
+
+#define BMAIO				0xA1
+
+#define SMB150_STATUS_REG	0x09
+#define SMB150_CTRL_REG		0x0a
+#define SMB150_CONF1_REG	0x0b
+#define RANGE_BWIDTH_REG	0x14
+#define SMB150_CONF2_REG	0x15
+
+/* IOCTLs*/
+#define BMA_IOCTL_INIT                  _IO(BMAIO, 0x31)
+#define BMA_IOCTL_WRITE                 _IOW(BMAIO, 0x32, char[5])
+#define BMA_IOCTL_READ                  _IOWR(BMAIO, 0x33, char[5])
+#define BMA_IOCTL_READ_ACCELERATION    _IOWR(BMAIO, 0x34, short[7])
+#define BMA_IOCTL_SET_MODE	  _IOW(BMAIO, 0x35, short)
+#define BMA_IOCTL_GET_INT	  _IOR(BMAIO, 0x36, short)
+#define BMA_IOCTL_GET_CHIP_LAYOUT	_IOR(BMAIO, 0x37, short)
+
+/* mode settings */
+#define BMA_MODE_NORMAL   	0
+#define BMA_MODE_SLEEP       	1
+
 /**
  * struct bma150_platform_data - data to set up bma150 driver
  *
